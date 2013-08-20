53ce554dd8bb0844c4ee948310f3f42470a9c4d7
Azkar <sdm350@gmail.com>
2013-08-20 07:07:01 -0400

    adding resolve loading

--- TransactionTracker/views.py
+++ TransactionTracker/views.py
@@ -19,26 +19,26 @@ def home(request):
 
     return render(request, 'index.html')
 
-    transactions = transaction.objects.all()
-
-    users = {1: {'paid': 0, 'owed': 0},
-             2: {'paid': 0, 'owed': 0}}
-
-    for trans in transactions:
-        if not trans.user.username in users:
-            users[trans.user.username] = {'paid': 0, 'owed': 0}
-
-        users[trans.user.username]['paid'] += trans.amount
-        users[trans.user.username]['owed'] += trans.amount * trans.transaction_type.multiplier
-
-    u2sum = users['Tom']['owed']
-    u1sum = users['Sean']['owed']
-
-    users['diff'] = u1sum - u2sum
-
-    money = users
-
-    return render(request, 'home.html', {"money": money})
+    # transactions = transaction.objects.all()
+    #
+    # users = {1: {'paid': 0, 'owed': 0},
+    #          2: {'paid': 0, 'owed': 0}}
+    #
+    # for trans in transactions:
+    #     if not trans.user.username in users:
+    #         users[trans.user.username] = {'paid': 0, 'owed': 0}
+    #
+    #     users[trans.user.username]['paid'] += trans.amount
+    #     users[trans.user.username]['owed'] += trans.amount * trans.transaction_type.multiplier
+    #
+    # u2sum = users['Tom']['owed']
+    # u1sum = users['Sean']['owed']
+    #
+    # users['diff'] = u1sum - u2sum
+    #
+    # money = users
+    #
+    # return render(request, 'home.html', {"money": money})
 
 
 def view(request):
@@ -48,22 +48,23 @@ def view(request):
 
 
 def add(request):
+    return render(request, 'add.html')
 
-    #adding new transaction
-    if request.method == "POST":
-        transaction_form = EntryForm(request.POST)
-        if transaction_form.is_valid():
-            user = User.objects.get(pk=transaction_form.data['user'])
-            trans_type = transaction_type.objects.get(pk=transaction_form.data['type'])
-
-            trans = transaction(user=user,
-                                amount=transaction_form.data['amount'],
-                                description=transaction_form.data['description'],
-                                transaction_type=trans_type)
-
-            trans.date = datetime.date.today()
-            trans.save()
-
-    transaction_form = EntryForm()
-
-    return render(request, 'add.html', {'t_form': transaction_form})
+    # #adding new transaction
+    # if request.method == "POST":
+    #     transaction_form = EntryForm(request.POST)
+    #     if transaction_form.is_valid():
+    #         user = User.objects.get(pk=transaction_form.data['user'])
+    #         trans_type = transaction_type.objects.get(pk=transaction_form.data['type'])
+    #
+    #         trans = transaction(user=user,
+    #                             amount=transaction_form.data['amount'],
+    #                             description=transaction_form.data['description'],
+    #                             transaction_type=trans_type)
+    #
+    #         trans.date = datetime.date.today()
+    #         trans.save()
+    #
+    # transaction_form = EntryForm()
+    #
+    # return render(request, 'add.html', {'t_form': transaction_form})
--- angular/static/moneytracker.js
+++ angular/static/moneytracker.js
@@ -1,4 +1,4 @@
-var moneytracker = angular.module("moneytracker", ['ngResource']);
+var moneytracker = angular.module("moneytracker", ['ngResource', 'moneyServices']);
 
 moneytracker.config(['$routeProvider',
 	function($routeProvider){
@@ -11,7 +11,17 @@ moneytracker.config(['$routeProvider',
 			.when ('/transactions',
 			{
 				controller: 'transactionController',
-				templateUrl: 'transactions/'
+				templateUrl: 'transactions/',
+				resolve: {
+					loadData: function(transactionResource) {
+						return transactionResource.getTransList().promise;
+					}
+				}
+			})
+			.when('/add',
+			{
+				controller: 'addController',
+				templateUrl: 'add/'
 			})
 			.otherwise({
 				redirectTo: '/'
@@ -24,25 +34,47 @@ moneytracker.controller('debtController', function($scope) {
 
 });
 
-moneytracker.controller('transactionController', function($scope, $resource) {
-	$scope.transList = $resource('/api/v1/transaction').get(function() {
+moneytracker.controller('addController', function($scope) {
+
+});
+
+var transactionController = moneytracker.controller('transactionController', function($scope, $resource) {
+
+//	init();
+	$scope.transResource = $resource('/api/v1/transaction');
+//	$scope.transactions = transactions;
+
+	function init() {
+		$scope.transList = $resource('/api/v1/transaction').get(parseResource);
+	}
+
+	$scope.NextPage = function() {
+		$scope.transList = $resource($scope.nextLink).get(parseResource);
+	};
+
+	$scope.PreviousPage = function() {
+		$scope.transList = $resource($scope.previousLink).get(parseResource);
+	};
+
+	$scope.GoToPage = function(page) {
+		$scope.transList = $resource('/api/v1/transaction/?limit=20&offset=' + $scope.pageSize * page)
+			.get(parseResource);
+	};
+
+	function parseResource() {
 		$scope.numberOfRecords = $scope.transList.meta.total_count;
 		$scope.pageSize = $scope.transList.meta.limit;
 		$scope.offset = $scope.transList.meta.offset;
 		$scope.numberOfPages = Math.ceil($scope.numberOfRecords/$scope.pageSize);
-	});
-
-	$scope.GoToPage = function(page) {
-		$scope.transList = $resource('/api/v1/transaction/?limit=20&offset=' + $scope.pageSize * page)
-			.get(function() {
-				$scope.numberOfRecords = $scope.transList.meta.total_count;
-				$scope.pageSize = $scope.transList.meta.limit;
-				$scope.offset = $scope.transList.meta.offset;
-				$scope.numberOfPages = Math.ceil($scope.numberOfRecords/$scope.pageSize);
-			}
-		);
+		$scope.nextLink = $scope.transList.meta.next;
+		$scope.previousLink = $scope.transList.meta.previous;
 	}
+
 });
+
+
+
+//moneytracker.controller('transactionController', transactionController);
 
 moneytracker.filter('range', function() {
 	return function(input, total) {
@@ -52,3 +84,15 @@ moneytracker.filter('range', function() {
 		return input;
 	};
 });
+
+
+angular.module('moneyServices', ['ngResource'])
+	.factory('transactionResource', function($resource) {
+		var transResource = $resource('/api/v1/transaction');
+
+		transResource.prototype.getTransList = function() {
+			return transResource.get();
+		};
+
+		return new transResource;
+	});
